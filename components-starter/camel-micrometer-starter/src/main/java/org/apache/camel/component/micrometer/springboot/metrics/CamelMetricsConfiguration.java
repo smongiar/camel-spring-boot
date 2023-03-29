1181c4c2a5ac11e209c6580a876856f4c82bd875
Tom Cunningham <tcunning@redhat.com>
2023-03-29 10:58:37 -0400

    Cherry picks of camel-spring-boot 3.20.2 -> 3.20.3 commits (#189)
    * CAMEL-18862: camel-spring-boot - No need for speical routes collector can use default
    
    * Add missing docs to tracingStandby
    
    * CAMEL-19133: camel-zookeeper - Serialize MetaData in camel-cloud service discovery problem. Thanks to geyipeng for patch.
    
    * CAMEL-19136: camel-micrometer-starter - Turn of metrics with uri tag by default as it can lead to too many tags due to dynamic values.
    
    * CAMEL-19168: camel-micrometer-starter - Make it possible to capture static uri path as tag
    
    ---------
    
    Co-authored-by: Claus Ibsen <claus.ibsen@gmail.com>

--- components-starter/camel-micrometer-starter/src/main/java/org/apache/camel/component/micrometer/springboot/metrics/CamelMetricsConfiguration.java
+++ components-starter/camel-micrometer-starter/src/main/java/org/apache/camel/component/micrometer/springboot/metrics/CamelMetricsConfiguration.java
@@ -22,6 +22,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class CamelMetricsConfiguration {
 
     /**
+     * Whether HTTP uri tags should be enabled or not in captured metrics.
+     * If disabled then the uri tag, is likely not able to be resolved and will be marked as UNKNOWN.
+     */
+    private boolean uriTagEnabled = true;
+
+    /**
+     * Whether to use static or dynamic values for URI tags in captured metrics.
+     *
+     * When using dynamic tags, then a REST service with base URL: /users/{id} will capture metrics
+     * with uri tag with the actual dynamic value such as: /users/123.
+     * However, this can lead to many tags as the URI is dynamic, so use this with care.
+     */
+    private boolean uriTagDynamic;
+
+    /**
      * Set whether to enable the MicrometerRoutePolicyFactory for capturing metrics
      * on route processing times.
      */
@@ -47,6 +62,14 @@ public class CamelMetricsConfiguration {
      * on the total number of routes and total number of routes running.
      */
     private boolean enableRouteEventNotifier = true;
+
+    public boolean isUriTagDynamic() {
+        return uriTagDynamic;
+    }
+
+    public void setUriTagDynamic(boolean uriTagDynamic) {
+        this.uriTagDynamic = uriTagDynamic;
+    }
 
     public boolean isEnableRoutePolicy() {
         return enableRoutePolicy;
