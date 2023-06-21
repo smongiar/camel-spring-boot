71267dbbb62739337b93b61d238baa8d2b124d95
Marco Carletti <mcarlett@redhat.com>
2023-06-21 14:11:51 +0200

    Avoid test execution of PlatformHttpBase class (#872)

--- components-starter/camel-platform-http-starter/src/test/java/org/apache/camel/component/platform/http/springboot/ServletPlatformHttpBase.java
+++ components-starter/camel-platform-http-starter/src/test/java/org/apache/camel/component/platform/http/springboot/ServletPlatformHttpBase.java
@@ -22,7 +22,7 @@ import org.assertj.core.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
-public class ServletPlatformHttpBase {
+abstract class ServletPlatformHttpBase {
 
 	@Autowired
 	private TestRestTemplate restTemplate;
